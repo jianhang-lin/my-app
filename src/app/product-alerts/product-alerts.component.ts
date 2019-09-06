8af7b87590f9d5c41dd340661b76f449ebc1265b
JianhangLin <linjianhang1010@gmail.com>
2019-09-06 14:11:10 +0800

    feat(product-alerts): add product field as input param

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-alerts.component.css']
 })
 export class ProductAlertsComponent implements OnInit {
-
+  // 指出其属性值是从组件的父组件（商品列表）中传入的
+  @Input() product;
   constructor() { }
 
   ngOnInit() {
