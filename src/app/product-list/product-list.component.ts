49853366f771ddf81d5117b134896ecbd2ea9705
JianhangLin <linjianhang1010@gmail.com>
2019-09-06 11:25:08 +0800

    feat(product-list): add share method

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import {products} from '../products';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent {
 
-  constructor() { }
+  products = products;
 
-  ngOnInit() {
+  share() {
+    window.alert('The product has been shared!');
   }
-
 }
