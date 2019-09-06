70eef34e0fdf94b854386f2ffae6f70b9efe2679
JianhangLin <linjianhang1010@gmail.com>
2019-09-06 16:07:13 +0800

    feat(app): import ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { RouterModule} from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
@@ -44,7 +45,8 @@ import { ShippingComponent } from './shipping/shipping.component';
         path: 'shipping',
         component: ShippingComponent
       }
-    ])
+    ]),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
