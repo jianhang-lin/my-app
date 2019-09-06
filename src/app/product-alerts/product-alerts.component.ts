460a6a8a32ac5adcc535c16928fab91f861a7c7b
JianhangLin <linjianhang1010@gmail.com>
2019-09-06 14:17:40 +0800

    feat(product-alerts): add notify filed to emit an event

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
@@ -9,6 +10,8 @@ import { Input } from '@angular/core';
 export class ProductAlertsComponent implements OnInit {
   // 指出其属性值是从组件的父组件（商品列表）中传入的
   @Input() product;
+  // 可以让商品提醒组件在notify属性发生变化时发出事件
+  @Output() notify = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
